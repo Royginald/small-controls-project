aad3d6ebe88ee6c4aee3f66c48bbe4a853e64d30
Royginald <77161110+Royginald@users.noreply.github.com>
2025-04-14 14:42:34 -0300

    Finished Sys ID script and added wiring info

--- Sys_ID/Sys_ID.ino
+++ Sys_ID/Sys_ID.ino
@@ -3,6 +3,7 @@
 #define TEST_VOLTAGE 6
 #define TEST_TIME 5
 #define NUM_DATA_POINTS 1000
+#define END_TOL 0.95
 
 #define MOTOR_1_PIN 5
 #define MOTOR_2_PIN 6
@@ -22,7 +23,8 @@ volatile long encoderPosition = 0;
 
 unsigned long loop_start_time, prog_start_time;
 long dt = TEST_TIME / NUM_DATA_POINTS;
-long data[NUM_DATA_POINTS];
+long data[NUM_DATA_POINTS]; // Steps
+float data_diff[NUM_DATA_POINTS - 1]; // Steps per second
 
 int i = 0;
 
@@ -61,11 +63,45 @@ void loop() {
     analogWrite(MOTOR_1_PIN, 0);
 
     // Process data
-    // Integrate
-    // find end value
-    // find time of 63%
-    // have tau and k_p
+    // Differentate
 
+    for (int j = 0; j < NUM_DATA_POINTS - 1; j++) {
+        data_diff[j] = ( data[j+1] - data[j] ) / dt;
+    }
+
+    float end_value = data_diff[NUM_DATA_POINTS-1] * END_TOL;
+
+    i = NUM_DATA_POINTS-1;
+    while(data_diff[i] > end_value){
+        i--;
+    }
+
+    float end_time = i * dt / END_TOL;
+    int end_index = i / END_TOL;
+
+    float average = 0;
+
+    for (int j = end_index; j < NUM_DATA_POINTS-1; j++) {
+        average += data_diff[j];
+    }
+
+    average /= NUM_DATA_POINTS - 1;
+
+    float K_p = average / TEST_VOLTAGE;
+
+    float rise_value = average * 0.63;
+
+    i = end_index;
+    while(data_diff[i] > rise_value) {
+        i--;
+    }
+
+    float tau = (dt) / (data_diff[i+1] - data_diff[i]) * (rise_value - data_diff[i]) + i * dt;
+
+    Serial.println("Plant gain: " + String(K_p) + " steps/s / V");
+    Serial.println("Plant rise time: " + String(tau) + " seconds");
+
+    delay(TEST_TIME);
 
 }
 
