74ba98ec2245831005f3595e602cffc62e18272d
MattTidd <mtidd2@unb.ca>
2025-06-27 11:09:28 -0300

    changed variables to be user-defined outside of header. added a print array function that hopefully works

--- Motor_test/control_MPC.h
+++ Motor_test/control_MPC.h
@@ -10,18 +10,15 @@ namespace MPC_Control {
     const float lambda = 1.1;   // move suppression lambda
 
     // manipulated dynamic matrix:
-    extern const float ppp[nu][N] = {0.0000377, 0.0000637, 0.0000815, 0.0000939, 0.0001023,
-                        0.0001082, 0.0001122, 0.0001149, 0.0001168, 0.0001182,
-                        0.0001191, 0.0001197, 0.0001201, 0.0001204, 0.0001206, 
-                        0.0001207, 0.0001208, 0.0001209, 0.0001210, 0.0001210};
+    extern const float ppp[nu][N];
 
     // state variables:
-    extern float y_hat[N][1] = 0.0;
-    extern float u_prev = 0.0;
+    extern float y_hat[N];
+    extern float u_prev;
 
     // functions:
     float control_MPC(float setpoint, float y_meas);
-    void init_MPC();
+    void print_Array(float arr[], int N);
 
 }   // MPC_Control
 
